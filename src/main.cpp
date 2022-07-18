b6453a81f65994a2876084cb7c4b585ac33accee
arcausin <82374375+arcausin@users.noreply.github.com>
2022-07-18 12:14:35 +0200

    librairies + smiley

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,33 @@
 #include "Arduino.h"
-//Constants
-#define NUM_JOY 2
-#define MIN_VAL 0
-#define MAX_VAL 1023
-//Parameters
-const int joyPin [2] = {A0, A1};
-const int joyBtn  = 2;
-const int joyOffset  = 0;
-//Variables
-int joyVal [NUM_JOY] = {0, 0};
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
 
-float joyRawToPhys(int raw) { /* function joyRawToPhys */
- ////Joystick conversion rule
- float phys = map(raw, MIN_VAL, MAX_VAL, -100 + joyOffset, 100 + joyOffset) - joyOffset;
- return phys;
-}
+int pinCS = 10; 
+//Din connects to pin 11
+//CLK connects to pin 13
 
-void readJoystick( ) { /* function readJoystick */
- ////Test routine for Joystick
- for (int i = 0; i < NUM_JOY; i++) {
-   joyVal[i] = analogRead(joyPin[i]);
-   // Serial.print(F("joy")); Serial.print(i); Serial.print(F(" : ")); Serial.println(joyVal[i]);
- }
- float yMove = joyRawToPhys(joyVal[1]);
- Serial.print(F("Conv")), Serial.print(1); Serial.print(F(" : ")); Serial.println(yMove);
- if(yMove == 100) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(12, LOW);
- }
- else if (yMove == -100) {
-  digitalWrite(12, HIGH);
-  digitalWrite(LED_BUILTIN, LOW);
- }
- else {
-  digitalWrite(12, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
- }
-//  for (int i = 0; i < NUM_JOY; i++) {
-//    Serial.print(F("Conv")), Serial.print(i); Serial.print(F(" : ")); Serial.println(joyRawToPhys(joyVal[i]));
-//  }
-//  if (!digitalRead(joyBtn)) {
-//    Serial.println(F("Joy Button pressed"));
-//  }
-}
+int numberOfHorizontalDisplays = 1;
+int numberOfVerticalDisplays = 1;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+const unsigned char PROGMEM smiley[] = {
+  B00000000,
+  B01000010,
+  B00000000,
+  B00011000,
+  B10000001,
+  B01000010,
+  B00111100,
+  B00000000
+};
 
 void setup() {
- //Init Serial USB
- Serial.begin(9600);
- Serial.println(F("Initialize System"));
- //Init Joystick
- for (int i = 0; i < NUM_JOY; i++) pinMode(joyPin[i], INPUT);
- // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(12, OUTPUT);
-}
-void loop() {
- readJoystick();
- delay(500);
+  matrix.setIntensity(10);
 }
 
+void loop() {
+  matrix.drawBitmap(0, 0, smiley, 8, 8, HIGH);
+  matrix.write();
+}
