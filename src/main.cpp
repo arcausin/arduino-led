72eb069062a3aec813e03507a0e2f54666ee42fc
arcausin <82374375+arcausin@users.noreply.github.com>
2022-07-18 11:24:43 +0200

    led droite gauche joystick

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,61 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
+//Constants
 #define NUM_JOY 2
 #define MIN_VAL 0
 #define MAX_VAL 1023
+//Parameters
 const int joyPin [2] = {A0, A1};
 const int joyBtn  = 2;
 const int joyOffset  = 0;
-bool isLightOn = false;
+//Variables
+int joyVal [NUM_JOY] = {0, 0};
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
-int increment1 = 0;
-void test()
-{
-  Serial.println(F("Joy Button pressed"));
-  // Serial.println(increment1);
-  increment1++;
+float joyRawToPhys(int raw) { /* function joyRawToPhys */
+ ////Joystick conversion rule
+ float phys = map(raw, MIN_VAL, MAX_VAL, -100 + joyOffset, 100 + joyOffset) - joyOffset;
+ return phys;
 }
 
-void setup()
-{
-  attachInterrupt(digitalPinToInterrupt(2), test, HIGH);
-  // initialize LED digital pin as an output.
-  Serial.begin(9600);
-  Serial.println(F("Initialize System"));
+void readJoystick( ) { /* function readJoystick */
+ ////Test routine for Joystick
+ for (int i = 0; i < NUM_JOY; i++) {
+   joyVal[i] = analogRead(joyPin[i]);
+   // Serial.print(F("joy")); Serial.print(i); Serial.print(F(" : ")); Serial.println(joyVal[i]);
+ }
+ float yMove = joyRawToPhys(joyVal[1]);
+ Serial.print(F("Conv")), Serial.print(1); Serial.print(F(" : ")); Serial.println(yMove);
+ if(yMove == 100) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(12, LOW);
+ }
+ else if (yMove == -100) {
+  digitalWrite(12, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
+ }
+ else {
+  digitalWrite(12, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+ }
+//  for (int i = 0; i < NUM_JOY; i++) {
+//    Serial.print(F("Conv")), Serial.print(i); Serial.print(F(" : ")); Serial.println(joyRawToPhys(joyVal[i]));
+//  }
+//  if (!digitalRead(joyBtn)) {
+//    Serial.println(F("Joy Button pressed"));
+//  }
+}
+
+void setup() {
+ //Init Serial USB
+ Serial.begin(9600);
+ Serial.println(F("Initialize System"));
+ //Init Joystick
+ for (int i = 0; i < NUM_JOY; i++) pinMode(joyPin[i], INPUT);
+ // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  for (int i = 0; i < NUM_JOY; i++) pinMode(joyPin[i], INPUT);
+  pinMode(12, OUTPUT);
 }
-
-void loop()
-{
-  delay(1000);
+void loop() {
+ readJoystick();
+ delay(500);
 }
 
